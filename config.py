38101d8dc35a69c104578f1919166077ee688062
Hamid Axtamov <hamidaxtamov@Hamids-MacBook-Air.local>
2025-05-05 18:34:54 +0500

    feat: configured the project basics

---
+++ config.py
@@ -0,0 +1,21 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import SecretStr, BaseModel
+
+class OpenAISettings(BaseModel):
+    API_KEY: SecretStr
+    MODEL: str = "gpt-4o-mini-2024-07-18"
+
+class PineconeSettings(BaseModel):
+    API_KEY: SecretStr
+    
+class Settings(BaseSettings):
+    OPENAI: OpenAISettings
+    PIPECONE: PineconeSettings
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_nested_delimiter="__",
+        env_file_encoding="utf-8",
+    )
+
+settings = Settings()
